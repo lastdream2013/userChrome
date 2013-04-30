5a5c091ee4bdc70de82db00ee5db358e678651b2
lastdream2013 <lastdream2013@hotmail.com>
2013-04-30 17:53:10 +0800

    Create blur2revertMod.uc.js

---
+++ blur2revertMod.uc.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        url bar blur to revert mod 
+// @namespace   blur2revert@zbinlin.gmail.com
+// @filename    blur2revert.uc.js
+// @description url bar blur to revert mod ( Ctrl + 鼠标点击页面后，恢复原来的地址 )
+// @author      zbinlin
+// @version     0.1.20110602.1
+// @updateURL     https://j.mozest.com/ucscript/script/39.meta.js
+// ==/UserScript==
+
+if (location == "chrome://browser/content/browser.xul") {
+  gBrowser.addEventListener("DOMWindowCreated", function () {
+		window.content.document.addEventListener("click", function (e) {
+			if (e.ctrlKey) {
+				document.getElementById("urlbar").handleRevert();
+			}
+		}, false);
+	}, false);
+}
