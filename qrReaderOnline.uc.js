c9317116ccf16c5115df4c588b9be8632b836f4e
lastdream2013 <lastdream2013@hotmail.com>
2013-06-27 17:18:32 +0800

    Update qrReaderOnline.uc.js

--- qrReaderOnline.uc.js
+++ qrReaderOnline.uc.js
@@ -1,11 +1,9 @@
 // ==UserScript==
 // @include        chrome://browser/content/browser.xul
 // @name           qrReaderOnline.uc.js
-// @version         0.14
-// @note            20130530 修正二维码内容为空时解析出错
-// @note            20130425 zxing.org返回数据又插了东西……再改 
-// @note            20130331 修正escapeHTML字符未转化的问题
-// @note            20130331 修正url判定正则表达式不准确
+// @version        0.15
+// @charset        UTF-8
+// @note           version 20130627 
 // @author         lastdream2013
 // ==/UserScript==
 
@@ -33,14 +31,19 @@ var qrReaderOnline={
                 if (req.status == 200) {
                    var title = req.responseText.match(/<title>(.+)<\/title>/i)[1];
                  	if (title =="Decode Succeeded") {
-                		var resultstr = req.responseText.match(/Parsed Result<\/td><td>(.+)<\/td><\/tr><\/table><hr\/>/i)[1];
-                 		resultstr = resultstr.match(/<pre[^>]+>(.*)<\/pre>/i)[1];
-                		//Services.console.logStringMessage('[ resultstr ]: ' + resultstr );
-                   		resultstr = resultstr.replace(/<br\/>/ig, '\n');
-						qrReaderOnline.read(qrReaderOnline.unescapeHTML(resultstr));
+                 		//Services.console.logStringMessage('[ req.responseText ]: ' + req.responseText );
+                 		try{
+	                		var resultstr = req.responseText.match(/Parsed Result<\/td><td>(.+)<\/td><\/tr><\/table><hr\/>/i)[1];
+	                 		resultstr = resultstr.match(/<pre[^>]+>(.*)<\/pre>/i)[1];
+	                   		resultstr = resultstr.replace(/<br\/>/ig, '\n');
+							qrReaderOnline.read(qrReaderOnline.unescapeHTML(resultstr));
+						}
+						catch(e){
+							alert(e);
+						}
 					}
 					else{
-						alert("\u89E3\u6790\u5931\u8D25\uFF1A" + title);
+						alert("解析失败：" + title);
 					}
                 }
             }
@@ -49,13 +52,13 @@ var qrReaderOnline={
 	read:function(d){
         	var p=/^(https?|ftp|file):\/\/[-_.~*'()|a-zA-Z0-9;:\/?,@&=+$%#]*[-_.~*)|a-zA-Z0-9;:\/?@&=+$%#]$/;
 		if(p.test(d)){
-			var f=confirm("QR\u7801\u4E3A\u7F51\u7EDC\u5730\u5740\uFF0C\u786E\u8BA4\u6253\u5F00?"+'\n\n'+d);
+			var f=confirm("QR码为网络地址，确认打开?"+'\n\n'+d);
 			if(f==true){
 				gBrowser.loadOneTab(d, null, null, null, true, false);
 	      	}
 		}
 		else{
-			var r=confirm("QR\u4EE3\u7801\u503C\uFF08\u6309OK\u952E\u5C06\u5176\u590D\u5236\u5230\u526A\u8D34\u677F\uFF09\uFF1A"+'\n\n'+d);
+			var r=confirm("QR代码值（按OK键将其复制到剪贴板）："+'\n\n'+d);
 			if(r==true){
 				try{
 					Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString(d);
@@ -71,7 +74,7 @@ var qrReaderOnline={
 		var menuItem=document.createElement("menuitem");
 			menuItem.setAttribute("id", "qrReaderOnline.menu");
 			menuItem.addEventListener("command", function(){qrReaderOnline.getClickHandler();}, false);
-			menuItem.setAttribute("label", "\u5728\u7EBF\u89E3\u6790\u56FE\u50CFQR\u7801");
+			menuItem.setAttribute("label", "在线解析图像QR码");
 			menuItem.setAttribute("class", "menuitem-iconic");
 			menuItem.setAttribute("image", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA3UlEQVQ4jaWSzWnDQBBGH2kiLsAnIfXhQzoKGBZcgHpxBWpBPqoAGdSCrJfTbix5V4Zk4GNmvvlddvCfgiqQsOdnY5HMJaYpGfulQa74uXCbt2qQm1Sa+GLvrbj3tN8NHg8/ICH6JX7L4TTp/e4BdBx1HD1A0Y/6E3SaxK7zBHq9+pXRWy7iBNp1Ytvq5eIZNATPkBB9Q1jFDMFv0LYVm0arSuvaGyRY1wk5zqrSphH7XofBI3iEZDsMKzvGn2Hfi8vy9nSLN7Es4jwXk3N/vxo2z/un/HYDN6f8F/kB7L2cwHXEHwAAAAAASUVORK5CYII=");
 		menu.insertBefore(menuItem, document.getElementById("context-openlinkintab"));
